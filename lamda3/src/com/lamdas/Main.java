36bd4a5de20eb75ddac78311a9905d158314539a
Armando Serrano <you@example.com>
2018-12-09 18:17:21 -0500

    alcance

--- lamda3/src/com/lamdas/Main.java
+++ lamda3/src/com/lamdas/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scopes scopes=new Scopes();
+        System.out.println(scopes.localVar());
+        System.out.println(scopes.StaticglobalsVar());
+
     }
 }
--- lamda3/src/com/lamdas/Scopes.java
+++ lamda3/src/com/lamdas/Scopes.java
@@ -1,11 +1,27 @@
 package com.lamdas;
 
+import jdk.nashorn.internal.objects.NativeInt16Array;
+
+import javax.imageio.ImageTranscoder;
+
 public class Scopes {
+    private static double atributo1;
+    private double atributo2;
     public double localVar(){
-        return  0;
+        double n3=3;
+        double n4=4;
+        //Operaciones operaciones = (t1,t2)->(t1*t2)+n3*n4;
+        Operaciones operaciones = (t1,t2)->{return (t1*t2)+n3*n4;};
+        return operaciones.prom(1,1);
+    }
+    public double StaticglobalsVar (){
+        Operaciones operaciones = (t1,t2)->{
+            atributo1=t1*t2+5;
+            atributo2=atributo1 *5;
+            return atributo2;
+        };
+        return operaciones.prom(1,5);
+
     }
 
-    public static void main(String[] args) {
-        // write your code here
-    }
 }
